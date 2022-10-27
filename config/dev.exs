6fefaa754c0263d05b90106c2b9dc007ddf75c75
Alexey Sokolov <alexvyber@gmail.com>
2022-10-27 22:23:07 +0300

    tailwind added

--- config/dev.exs
+++ config/dev.exs
@@ -27,7 +27,8 @@ config :csv, CsvWeb.Endpoint,
   secret_key_base: "Epu+Tq8g3DIH3kjO6pLqb21S2PhyEQUyW1qkO8UlHruIlZU+ozsaYrry8M/5h5s8",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
